f3b843fea5532912b9a52c2fbace91f7ff60a0b3
wadashuri <wada20010108@icloud.com>
2023-01-31 23:43:50 +0900

    修正

--- server/resources/ts/pages/calendar/index.tsx
+++ server/resources/ts/pages/calendar/index.tsx
@@ -2,27 +2,15 @@ import React from "react";
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import jaLocale from '@fullcalendar/core/locales/ja';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import listPlugin from '@fullcalendar/list';
 
 const CalendarPage: React.VFC = () => {
     return (
-    <div>
+      <div>
       <FullCalendar
-        plugins={[dayGridPlugin, timeGridPlugin, listPlugin]} // 追加
+        plugins={[dayGridPlugin]}
         initialView="dayGridMonth"
-        locales={[jaLocale]}
-        locale='ja'
-        headerToolbar={{
-          left: 'prev,next today',
-          center: 'title',
-          right: 'dayGridMonth,timeGridWeek listWeek', // 追加
-        }}
-        events={[
-        { title: 'eventを', start: '2023-03-14' },
-          {title:'event2', start: '2023-03-14'},
-          {title:'こんな感じで追加できます', start: '2023-03-15', end: '2023-03-17'}
-        ]}
+        locales={[jaLocale]}         // 追加
+        locale='ja'                  // 追加
       />
     </div>
     );
