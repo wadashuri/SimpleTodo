b2548e91692d56ac340b13587e54279d9cd8fa35
wadashuri <wada20010108@icloud.com>
2023-03-27 02:22:09 +0900

    かるい修正

--- server/resources/ts/pages/help/index.tsx
+++ server/resources/ts/pages/help/index.tsx
@@ -4,7 +4,7 @@ const HelpPage: React.VFC = () => {
     return (
         <div className='align-center'>
             <h1>ヘルプ</h1>
-            <p>使い方を解説します<br/>
+            <p>使い方を解説します</p><br/>
             このサイトはログインが必要です
         </div>
     );
